06c9ebd6f28884d5e5cfc797a9bb8e399fef731b
Fuat Aydın <GitHub_Email_Adresi>
2023-11-08 14:17:25 +0300

    added FluentValidation

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,7 @@
+using ETicaretAPI.Application.Validators.Products;
+using ETicaretAPI.Infrastructure.Filters;
 using ETicaretAPI.Persistence;
+using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +14,11 @@ policy.WithOrigins("http://localhost:4200", "https://localhost:4200").AllowAnyHe
 ));
 
 builder.Services.AddPersistanceServices();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options=> options.Filters.Add<ValidationFilters>())
+.AddFluentValidation(configuration => configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+.ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
